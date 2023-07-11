f195b7f327f8b97146553154d4719b4e1133c9fe
sevase <kotaseva8@gmail.com>
2023-07-11 13:48:52 +0300

    менюшка

--- osn_calc.py
+++ osn_calc.py
@@ -6,14 +6,16 @@ root = Tk()
 root.title('Calculator')
 root.geometry('350x500')
 
+# функция меню, для перехода в калькуляторы: массы и программиста
+def menu_main():
+    main_menu = Menu()
+    main_menu.add_cascade(label="Вес"''' command=open_weight_window''')
+    main_menu.add_cascade(label="Программист" '''command=open_programming_window''')
+    root.config(menu=main_menu)
 
-main_menu = Menu()
-main_menu.add_cascade(label="Вес"''' command=open_weight_window''')
-main_menu.add_cascade(label="Программист" '''command=open_programming_window''')
 
 
-
-root.config(menu=main_menu)
+menu_main()
 root.mainloop()
 
 
