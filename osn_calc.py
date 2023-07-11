5a22afc23aa2d39080380a9d0525437a7c317cb1
sevase <kotaseva8@gmail.com>
2023-07-11 16:55:57 +0300

    сделал основу кнопок главного калькулятор

--- osn_calc.py
+++ osn_calc.py
@@ -34,26 +34,60 @@ def knopki():
     btn3.place(x=170, y=70, width=70, height=70)
 
     # +
-    btn4 = Button(window, bg='black', fg='white', text='+', font=('', 15))
-    btn4.place(x=242, y=70, width=70, height=70)
+    btn_plus = Button(window, bg='black', fg='white', text='+', font=('', 20))
+    btn_plus.place(x=242, y=70, width=70, height=70)
 
 
     ### вторая строчка кнопок
     # 4
-    btn1 = Button(window, bg='black', fg='white', text='4', font=('', 15))
-    btn1.place(x=25, y=142, width=70, height=70)
+    btn4 = Button(window, bg='black', fg='white', text='4', font=('', 15))
+    btn4.place(x=25, y=142, width=70, height=70)
 
     # 5
-    btn2 = Button(window, bg='black', fg='white', text='5', font=('', 15))
-    btn2.place(x=98, y=142, width=70, height=70)
+    btn5 = Button(window, bg='black', fg='white', text='5', font=('', 15))
+    btn5.place(x=98, y=142, width=70, height=70)
 
     # 6
-    btn3 = Button(window, bg='black', fg='white', text='6', font=('', 15))
-    btn3.place(x=170, y=142, width=70, height=70)
+    btn6 = Button(window, bg='black', fg='white', text='6', font=('', 15))
+    btn6.place(x=170, y=142, width=70, height=70)
 
     # -
-    btn4 = Button(window, bg='black', fg='white', text='-', font=('', 15))
-    btn4.place(x=242, y=142, width=70, height=70)
+    btn_minus = Button(window, bg='black', fg='white', text='-', font=('', 20))
+    btn_minus.place(x=242, y=142, width=70, height=70)
+
+    ### третья строчка кнопок
+    # 7
+    btn7 = Button(window, bg='black', fg='white', text='7', font=('', 15))
+    btn7.place(x=25, y=214, width=70, height=70)
+
+    # 8
+    btn8 = Button(window, bg='black', fg='white', text='8', font=('', 15))
+    btn8.place(x=98, y=214, width=70, height=70)
+
+    # 9
+    btn9 = Button(window, bg='black', fg='white', text='9', font=('', 15))
+    btn9.place(x=170, y=214, width=70, height=70)
+
+    # *
+    btn_umn = Button(window, bg='black', fg='white', text='x', font=('', 20))
+    btn_umn.place(x=242, y=214, width=70, height=70)
+
+    ### третья строчка кнопок
+    # %
+    btn7 = Button(window, bg='black', fg='white', text='%', font=('', 15))
+    btn7.place(x=25, y=286, width=70, height=70)
+
+    # 0
+    btn8 = Button(window, bg='black', fg='white', text='0', font=('', 15))
+    btn8.place(x=98, y=286, width=70, height=70)
+
+    # ÷
+    btn9 = Button(window, bg='black', fg='white', text='÷', font=('', 20))
+    btn9.place(x=170, y=286, width=70, height=70)
+
+    # =
+    btn_umn = Button(window, bg='red', fg='white', text='=', font=('', 20))
+    btn_umn.place(x=242, y=286, width=70, height=70)
 
 knopki()
 menu_main()
